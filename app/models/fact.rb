d8f08a687dd792cfc43cde215a1c00677eb3467f
John Brunton <jbrunton@zipcar.com>
2015-05-16 14:27:21 +0100

    link to references from facts

--- app/models/fact.rb
+++ app/models/fact.rb
@@ -1,3 +1,11 @@
 class Fact < ActiveRecord::Base
   belongs_to :reference
+
+  def reference_keys
+    references.split(",").map(&:strip)
+  end
+
+  def reference_records
+    Reference.where(key: reference_keys)
+  end
 end
