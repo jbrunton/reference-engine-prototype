3c950efb0ad4f137ba60bff15073a6fa636acd62
John Brunton <jbrunton@zipcar.com>
2015-05-16 13:42:38 +0100

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Reference.create(
+    key: 'Econ:PaternityLeave:2014',
+    title: 'Why Swedish men take so much paternity leave',
+    summary: 'ALONG with its Nordic neighbours, Sweden features near the top of most gender-equality rankings. The World Economic Forum rates it as having one of the narrowest...',
+    author: 'The Economist',
+    url: 'http://www.economist.com/blogs/economist-explains/2014/07/economist-explains-15'
+)
+
+Fact.create(
+    summary: 'In Sweden, after introducing equal parental leave, only 0.4% of men took up the offer in the first year.',
+    references: 'Econ:PaternityLeave:2014',
+    categories: 'Evidence/SocialNorms'
+)
