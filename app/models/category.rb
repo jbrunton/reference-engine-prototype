201b4c2ec9451b3e156d4fef43df995e2bf2f5e1
John Brunton <jbrunton@zipcar.com>
2015-05-17 12:42:42 +0100

    generate category parents correctly

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,35 +1,20 @@
 class Category < ActiveRecord::Base
   belongs_to :category
-  #belongs_to :parent, class_name: "Category"
+  belongs_to :parent, class_name: "Category"
   has_and_belongs_to_many :facts
 
-  # before_save :assign_parent
-  #
-  # def assign_parent
-  #   puts "*** assign_parent (#{name})"
-  #   parent_name, *rest = name.split('/')
-  #   puts "*** parent_name (#{parent_name})"
-  #   unless rest.empty?
-  #     parent = Category.where(name: parent_name).first_or_create
-  #     puts "*** parent: (#{parent.name})"
-  #   end
-  # end
+  after_commit :assign_parent
+
+  def assign_parent
+    parent_name = category_path[0..-2].join('/')
+    unless parent_name.empty?
+      self.parent = Category.where(name: parent_name).first_or_create
+      save if changes['parent_id']
+    end
+  end
 
   def category_path
     name.split('/')
-  end
-
-  #
-  # def ancestors
-  #   *ancestors, _ = category_parts
-  #   ancestors
-  # end
-
-  def parent
-    @parent ||= begin
-      parent_name = category_path[0..-2].join('/')
-      Category.find_by(name: parent_name)
-    end
   end
 
   def ancestors
