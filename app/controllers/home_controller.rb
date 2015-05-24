36f5502ae76cf3189dd0bc213e54d065b875cb0b
John Brunton <jbrunton@zipcar.com>
2015-05-24 01:06:01 +0100

    #5 simplify homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @categories = Category.all.sort_by { |cat| cat.name }
+    categories = Category.all.sort_by { |cat| cat.name }
+
+    selectable_categories = categories.select{ |cat| !cat.facts.empty? }
+    @selected_category = selectable_categories[rand(selectable_categories.length)]
+
+    @root_categories = categories.select{ |cat| cat.parent.nil? }
   end
 end
