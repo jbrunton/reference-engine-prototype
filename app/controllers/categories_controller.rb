da19f82c2562069615d805ab762f857b2c8910c8
John Brunton <jbrunton@zipcar.com>
2015-05-24 10:24:24 +0100

    #7 bunch of UX improvements

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find_by_name(params[:category])
+    @root_categories = Category.where(parent: nil)
   end
 end
